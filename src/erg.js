52908a066cc07c7d07a159f944d09f15e176d36a
jwwishart <jwwishart>
2015-08-27 22:43:41 +1000

    Continued trying to simplify parser
    - Bit Better Structure.
    - Seems like too much code still... should be simpler
    than this

--- src/erg.js
+++ src/erg.js
@@ -81,9 +81,59 @@ var erg;
         }
     }
 
+    function generate_global_constants(setName, arr) {
+        if (!erg._global_constants) {
+            erg._global_constants = [];
+        }
+
+        erg._global_constants[setName] = erg._global_constants[setName] || {};
+
+        var i = 0;
+
+        for (var k in arr) {
+            erg._global_constants[setName][arr[k]] = __global[arr[k]] = i++;
+            erg._global_constants[setName][k] = arr[k];
+        }
+    }
+
+    function get_global_constant_name (setName, number) {
+        return erg._global_constants[setName][number];
+    }
+
 
     // @Exports ---------------------------------------------------------------
     //  
+
+    var CompilerContext = function(program, files, options) {
+        this.program = program;
+        this.files = files;
+        this.options = options;
+        this.logger = options.logger;
+
+        this.current_filename = '';
+        this.current_code = '';
+    };
+
+    erg.CompilerContext = CompilerContext;
+
+    erg.CompilerContext.prototype.log = function(group, message) {
+        if (this.logger) {
+            this.logger.log(group, message);
+        }
+    };
+
+    erg.CompilerContext.prototype.error = function(group, error) {
+        if (this.logger) {
+            this.logger.error(group, error);
+        }
+    };
+
+    erg.CompilerContext.prototype.warning = function(group, warning) {
+        if (this.logger) {
+            this.logger.error(group, warning);
+        }
+    };
+
 
     /// Compiles a file or files
     ///
@@ -98,15 +148,19 @@ var erg;
 
         // TODO(jwwishart) in debug dump this ast.
         var program = new Program();
+        var context = new erg.CompilerContext(program, '', options);
 
         each(files, function(code, filename) {
-            result +=
-                erg.target(
-                    erg.parse(
-                        filename,
-                        program,
-                        erg.tokenize(
-                            erg.scan(filename, code))));
+            // Set current file information to context
+            context.current_filename = filename;
+            context.current_code     = code;
+
+            // Process the file!
+            var scanner = erg.scan(context);
+            var tokenizer = erg.tokenize(context, scanner);
+            var ast = erg.parse(context, tokenizer);
+
+            result += erg.target(context, ast) + "\n\n\n";
         });
 
         return result;
@@ -116,12 +170,24 @@ var erg;
     // @Scanner ---------------------------------------------------------------
     //
 
-    erg.scan = function(filename, code) {
-        var i = 0;
-        var firstCol = 1;
+    var Lexeme = function(filename, line_no, col_no) {
+        this.filename = filename;
+        this.line_no = line_no;
+        this.col_no = col_no;
+    };
 
-        var lineNo = 1;
-        var colNo = firstCol;
+    erg.Lexeme = Lexeme;
+
+
+    erg.scan = function(context) {
+        var filename = context.current_filename;
+        var code = context.current_code;
+
+        var i = 0;
+        var first_col = 1;
+
+        var line_no = 1;
+        var col_no = first_col;
 
         return {
             peek: function() {
@@ -132,24 +198,21 @@ var erg;
                 return null;
             },
             eat: function() {
+                context.log("SCANNER", ": " + code[i]);
+
                 // NOTE: Do the line and char stuff when you eat, not 
                 // before!
                 if (code[i] === '\n') {
-                    lineNo++;
-                    colNo = firstCol - 1; // we will increment by one below so we need to remove that here!
+                    line_no++;
+                    col_no = first_col - 1; // we will increment by one below so we need to remove that here!
                 }
 
                 i++;
-                colNo++;
+                col_no++;
             },
-
-            get_position_info: function() {
-                return {
-                    lineNo: lineNo,
-                    colNo: colNo,
-                    filename: filename
-                };
-            }
+            get_lexeme: function() {
+                return new Lexeme(filename, line_no, col_no, this.peek());
+            },
         };
     };
 
@@ -157,7 +220,58 @@ var erg;
     // @Tokenizer -------------------------------------------------------------
     //
 
-    erg.tokenize = function(scanner) {
+    generate_global_constants('TOKEN_TYPES', [
+        'TOKEN_TYPE_UNKNOWN', // TODO(jwwishart) what? why would we need this... why not throw an exception
+        'TOKEN_TYPE_NULL',
+        
+        'TOKEN_TYPE_WHITESPACE',
+        'TOKEN_TYPE_COMMENT_SINGLE_LINE',
+
+        'TOKEN_TYPE_IDENTIFIER',
+
+        // Literals
+        'TOKEN_TYPE_STRING_LITERAL',
+        'TOKEN_TYPE_NUMBER_LITERAL',
+        'TOKEN_TYPE_BOOLEAN_LITERAL',
+
+        // Punctuation
+        'TOKEN_TYPE_PAREN_OPEN',
+        'TOKEN_TYPE_PAREN_CLOSE',
+        'TOKEN_TYPE_BRACE_OPEN',
+        'TOKEN_TYPE_BRACE_CLOSE',
+
+        'TOKEN_TYPE_SEMICOLON',
+
+        'TOKEN_TYPE_SINGLE_COLON',
+        'TOKEN_TYPE_DOUBLE_COLON',
+        'TOKEN_TYPE_COLON_EQUALS',
+
+        'TOKEN_TYPE_COMMA',
+
+        // Keywords
+        'TOKEN_TYPE_KEYWORD_RETURN',
+
+        // Symbols
+        'TOKEN_TYPE_PLUS',
+        'TOKEN_TYPE_DOT',
+
+        'TOKEN_TYPE_ASSIGNMENT',
+        'TOKEN_TYPE_UNINITIALIZE_OPERATOR',
+        'TOKEN_TYPE_ASM_BLOCK',
+        'TOKEN_TYPE_DIRECTIVE',
+        'TOKEN_TYPE_KEYWORD_DEFER'
+    ]);
+
+    var Token = function(type, lexeme) {
+        this.type = type;
+        this.typeName = get_global_constant_name('TOKEN_TYPES', type);
+        this.lexeme = lexeme;
+        this.text = lexeme.text;
+    }
+
+    erg.Token = Token;
+
+    erg.tokenize = function(context, scanner) {
         var CharToToken = {
             ' '  : TOKEN_TYPE_WHITESPACE,
             '\t' : TOKEN_TYPE_WHITESPACE,
@@ -194,26 +308,17 @@ var erg;
             scanner.eat();
         }
 
+        // TODO(jwwishart) can we get rid of this?
         function set_location(token) {
-            var pos = scanner.get_position_info();
-            token.lineNo = pos.lineNo;
-            token.colNo = pos.colNo;
-            token.filename = pos.filename;
+            var lexeme = scanner.get_lexeme();
+
+            token.lexeme = lexeme;
         }
 
-        function create_token(type, text) {
-            var pos = scanner.get_position_info();
+        function create_token(type) {
+            var lexeme = scanner.get_lexeme();
 
-            return {
-                type: type,
-                typeName: get_global_constant_name('TOKEN_TYPES', type),
-                text: text,
-
-                // Position
-                filename: pos.filename
-                lineNo: pos.lineNo,
-                colNo: pos.colNo,
-            };
+            return new Token(type, lexeme);
         }
 
 
@@ -513,7 +618,7 @@ var erg;
                     }
 
                     if (c === ':') {
-                        colNo = scanner.get_position_info().colNo;
+                        colNo = scanner.get_lexeme().colNo;
 
                         eat();
 
@@ -596,9 +701,9 @@ var erg;
                         return token;
                     }
 
-                    var errorCharInfo = scanner.get_position_info();
+                    var error_char_info = scanner.get_position_info();
 
-                    throw new Error("ERROR: Unexpected character '" + c + "' (ASCII: ' + c.charCodeAt(0) + ') (ln: " + errorCharInfo.lineNo + ", col: " + errorCharInfo.colNo +")");
+                    throw new Error("ERROR: Unexpected character '" + c + "' (ASCII: ' + c.charCodeAt(0) + ') (ln: " + error_char_info.line_no + ", col: " + error_char_info.col_no +")");
                 }
 
                 return null;
@@ -614,19 +719,12 @@ var erg;
     // @Parser ----------------------------------------------------------------
     //
 
-    /// Parses tokens and constructs an ast on the program
-    /// ast node that is passed in.
-    ///
-    /// Parameters:
-    ///     files   : a map of filename > code
-    ///     options : a map of options for the complier 
-    ///
-    /// Returns:
-    ///     The compiled output (JavaScript in this case)
-    erg.parse = function(filename, program, tokenizer) {
-        
-        // Helper Functions
-        //
+    (function() {
+        // So we don't have to keep parsing these things around!!!
+
+        var context = null;
+        var tokenizer = null;
+
 
         function peek() {
             return tokenizer.peek();
@@ -636,43 +734,184 @@ var erg;
             tokenizer.eat();
         }
 
+        function iterate(func, till) {
+            till = till || null;
 
-        // File Node Construction
-        //
-
-        var file = new File(filename, program);
-        program.files.push(file);
-
-
-        // Construct Context helper object
-        // 
-
-
-// UPTO HERE
-// UPTO HERE
-// UPTO HERE
-// UPTO HERE
-// UPTO HERE
-        var context = {
-            filename: filename,
-            program: program,
-            tokenizer: tokenizer
-        };
-
-        // Parse File
-        //
-
-        var scope = file.scope;
-        while (peek() !== null) {
-            parse_scope(scope, tokenizer);
+            while (tokenizer.peek() !== till) {
+                func();
+            }
         }
-    }
+
+        function accept(token_type) {
+            if (peek() === null) {
+                return false; // Not the requested token
+            }
+
+            if (is_array(token_type)) {
+                var found = false;
+
+                each(token_type, function() {
+                    if (peek().type === this) {
+                        found = true;
+                        return false;
+                    }
+                });
+
+                if (found === true) return true;
+            } else {
+                if (peek().type === token_type) {
+                    return true; // The requested token
+                }
+            }
+
+            return false; // Not the requested token
+        }
+
+        function expect(token_type) {
+            if (peek() === null) {
+                throw new Error("Unexpected end of file. Was expecting: " + JSON.stringify(token_type));
+            }
+
+
+            if (is_array(token_type)) {
+                var found_match = false;
+
+                each(token_type, function() {
+                    if (peek().type === this) {
+                        found = true;
+                        return false;
+                    }
+                });
+
+                if (found === true) return true;
+
+                // TODO(jwwishart) make this error a little clearer by showing the ACTUAL string constant names for all required tokens
+                throw new Error('Unexpected Token ' + peek().type + ' (' + get_global_constant_name('TOKEN_TYPES', peek().type) + ') was expecting one of ' + JSON.stringify(token_type) + ' Token Info: ' + JSON.stringify(peek()));
+            } else {
+                if (peek().type !== token_type) {
+                    throw new Error('Unexpected Token ' + peek().type + ' (' + get_global_constant_name('TOKEN_TYPES', peek().type) + ') was expecting ' + token_type + ' (' + get_global_constant_name('TOKEN_TYPES', token_type) + ') Token Info: ' + JSON.stringify(peek()));
+                }
+            }            
+        }
+
+        /// Parses tokens and constructs an ast on the program
+        /// ast node that is passed in.
+        ///
+        /// Parameters:
+        ///     files   : a map of filename > code
+        ///     options : a map of options for the complier 
+        ///
+        /// Returns:
+        ///     The compiled output (JavaScript in this case)
+        erg.parse = function(context_arg, tokenizer_arg) {
+            context = context_arg;
+            tokenizer = tokenizer_arg;
+
+
+            // Helper Functions
+            //
+
+
+            // File Node Construction
+            //
+
+            var file = new File(context.current_filename, context.program);
+            context.program.files.push(file);
+
+
+            // Parse File
+            //
+
+            var scope = file.scope;
+
+            iterate(function() {
+                parse_scope(scope);
+            });
+        }
+
+        function parse_scope(current_scope) {
+            while (peek() != null) {
+                // {
+                if (accept(TOKEN_TYPE_BRACE_OPEN)) {
+                    eat();
+                    parse_scope(new Scope(current_scope));
+                    continue;
+                }
+
+                // }
+                if (accept(TOKEN_TYPE_BRACE_CLOSE)) {
+                    eat();
+                    return;
+                }
+
+                // Statements
+                parse_statement(current_scope);
+            }
+        }
+
+        function parse_statement(current_scope) {
+            if (accept(TOKEN_TYPE_IDENTIFIER)) {
+                (function() {
+                    var token = peek();
+                    var identifier = token.lexeme.text;
+
+                    eat(); // identifier
+
+                    // Variable Declaration (with or without type)
+                    //
+
+                    if (accept([TOKEN_TYPE_SINGLE_COLON, TOKEN_TYPE_COLON_EQUALS])) {
+                        (function() {
+                            var variableDecl = new VariableDeclaration(identifier);
+                            var data_type_name = 'any';
+
+                            //variableDecl.tokens.push(token);
+                            //variableDecl.tokens.push(peek());
+
+
+                            // Type declared or inferred
+                            //
+
+                            if (accept(TOKEN_TYPE_COLON_EQUALS)) {
+                                eat();
+                            } else if (accept(TOKEN_TYPE_SINGLE_COLON)) {
+                                eat(); // :
+
+                                // data_type?
+                                if (expect(TOKEN_TYPE_IDENTIFIER)) {
+                                    data_type_name = peek().lexeme.text;
+
+                                    eat();
+
+                                    // TODO(jwwishart) does the type exist. Add to scope and program, mark as unknown type
+                                }
+                            }
+
+                            
+
+ 
+
+                        }());
+                    }
+                    
+                }());
+            }
+        }
+
+        function parse_variable_declaration(current_scope) {
+            if (accept(TOKEN_TYPE_SINGLE_COLON)) {
+            }
+        }
+
+
+    }());
 
 
     // @Ast Nodes -------------------------------------------------------------
     //
 
     function AstNode() {
+        this.tokens = [];
     }
 
 
@@ -684,27 +923,68 @@ var erg;
         }
 
         this.files = [];
-        this.compileOptions = {};
+        this.types = [
+            // NOTE(jwwishart) the default value is RAW as we 
+            new DataType('any',     null,   true),
+            new DataType('string',  '',     true),
+            new DataType('int',     0,      true),
+            new DataType('float',   0.0,    true),
+        ];
     }
     Program.prototype = new AstNode();
 
 
     function File(name, program) {
-        this.name = name;
+        this.filename = name;
         this.program = program;
 
-        this.scope = new Scope(this);
+        this.parent = program;
+
+        this.types = [];
     }
-    File.prototype = new AstNode();
+    File.prototype = new Scope();
 
 
     function Scope(parent) {
-        this.parent = parent;
-
+        this.parent = parent; // only need to go UP the scope, not down...
+        
         this.statements = [];
         this.deferred = [];
+
+        this.types = [];
     }
     Scope.prototype = new AstNode();
+
+
+    ///
+    /// Arguments
+    ///     identifier      : the name of the variable
+    ///     variable_type   : variable type (var, const)
+    ///     data_type       : the data type as TypeDefinition
+    function VariableDeclaration(identifier, variable_type, data_type, init) {
+        this.identifier = identifier;
+        this.variable_type = variable_type || 'variable'; // variable or constant
+        this.data_type = data_type || 'any';
+        this.init = init || [];
+    }
+    VariableDeclaration.prototype = new AstNode();
+
+
+
+    // @Data Types ------------------------------------------------------------
+    //
+
+    function DataType(name, default_value, is_builtin) {
+        this.name = name;
+        this.default_value = default_value || null;
+        this.is_builtin = is_builtin || false;
+    }
+
+    function Literal(value, data_type) {
+        this.value = value;
+        this.data_type = data_type || new DataType('any', null, true);
+    }
+
 
 
 
