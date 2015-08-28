cb05efc587074cff83edd62fb619eb945a716a3b
jwwishart <jwwishart>
2015-08-28 22:22:20 +1000

    Cleanup and got working on this mac

--- demo/testing.js
+++ demo/testing.js
@@ -1,9 +1,9 @@
-// Generated: 2015-8-28 21:49
+// Generated: 2015-8-28 22:19
 // File Start: input_filename
     var num = null;
     var _num = null;
     var num2 = null;
-    var num2 = null;
+    var num_const = null;
 // File End: input_filename
 
 
